a6672d0d18f136927ea40533bcf08422403e7b98
Engin <denganjun1234@qq.com>
2015-12-24 02:22:22 +0800

    base 2 2015-12-24 2:22:21

--- src/swiper.js-demo/demo1.js
+++ src/swiper.js-demo/demo1.js
@@ -2,23 +2,16 @@
 
 
     var tab = $('div.en-tab'),
-        tabNavItems = tab.find('ul.en-tab-nav>li')
-
-    var tabContentSwiper = new Swiper(tab.find('.en-tab-content'), {
-        // pagination: '.swiper-pagination',
-        // paginationClickable: true
-        onSlideChangeStart: function(swiper) {
-            // console.log(swiper)
-            var index = swiper.activeIndex
-            console.log(index)
-            tabNavItems.removeClass('active')
-                .eq(index).addClass('active')
-
-        }
-    });
+    	tabHeader = tab.children('.en-tab-header'),
+        tabNavItems = tab.find('ul.en-tab-nav>li'),
+        tabContent = tab.children('.en-tab-content')
 
 
-    var tabHeaderSwiper = new Swiper(tab.find('.en-tab-header'), {
+    tabNavItems.each(function (index) {
+    	tabNavItems.eq(index).data('tabIndex', index)
+    })
+
+    var tabHeaderSwiper = new Swiper(tabHeader, {
         // scrollbar: '.swiper-scrollbar',
         direction: 'horizontal',
         slidesPerView: 'auto',
@@ -28,4 +21,43 @@
         // freeModeMomentum: false,
     });
 
+    var tabContentSwiper = new Swiper(tabContent, {
+        // pagination: '.swiper-pagination',
+        // paginationClickable: true
+        onSlideChangeStart: function(swiper) {
+            // console.log(swiper)
+            var index = swiper.activeIndex,
+            	activCls = 'active'
+            tabNavItems.removeClass(activCls).eq(index).addClass(activCls)
+
+            tabHeaderSwiper.slideTo(index-1)
+        }
+    });
+
+    tabHeader.on('tap', 'ul.en-tab-nav li', function () {
+    	var index = $(this).data('tabIndex')
+    	tabContentSwiper.slideTo(index)
+    })
+
+
+    var scrollY = tabContent.find('div.scroll-y');
+    scrollY.each(function (index) {
+    	// console.log(scrollY.eq(index)[0])
+    	var swiper = new Swiper(scrollY.eq(index), {
+    	    // scrollbar: '.swiper-scrollbar',
+    	    direction: 'vertical',
+    	    slidesPerView: 'auto',
+    	    mousewheelControl: true,
+    	    freeMode: true
+    	});
+
+    	scrollY.eq(index).data('swiper', swiper)
+    })
+
+
+// setTimeout(function () {
+	
+// alert(scrollY.eq(0)[0].clientHeight)
+// alert(scrollY.eq(0).find('.swiper-slide')[0].clientHeight)
+// }, 2000)
 }();

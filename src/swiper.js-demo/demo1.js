41f96e2aeccb62ceb5848682d66311b014cdd792
Engin <denganjun1234@qq.com>
2015-12-24 11:25:22 +0800

    假如滚动条；使用hammer js 解决zepto tap乱触发的问题；

--- src/swiper.js-demo/demo1.js
+++ src/swiper.js-demo/demo1.js
@@ -1,14 +1,14 @@
-! function() {
+! function(window, document, $, Hammer) {
 
 
     var tab = $('div.en-tab'),
-    	tabHeader = tab.children('.en-tab-header'),
+        tabHeader = tab.children('.en-tab-header'),
         tabNavItems = tab.find('ul.en-tab-nav>li'),
         tabContent = tab.children('.en-tab-content')
 
 
-    tabNavItems.each(function (index) {
-    	tabNavItems.eq(index).data('tabIndex', index)
+    tabNavItems.each(function(index) {
+        tabNavItems.eq(index).data('tabIndex', index)
     })
 
     var tabHeaderSwiper = new Swiper(tabHeader, {
@@ -27,37 +27,78 @@
         onSlideChangeStart: function(swiper) {
             // console.log(swiper)
             var index = swiper.activeIndex,
-            	activCls = 'active'
+                activCls = 'active';
             tabNavItems.removeClass(activCls).eq(index).addClass(activCls)
 
-            tabHeaderSwiper.slideTo(index-1)
+            tabHeaderSwiper.slideTo(index - 1)
         }
     });
 
-    tabHeader.on('tap', 'ul.en-tab-nav li', function () {
-    	var index = $(this).data('tabIndex')
-    	tabContentSwiper.slideTo(index)
+    // zepto tap 在拖动很远的时候，也被触发了
+    // tabHeader.on('tap', 'ul.en-tab-nav li', function() {
+    //     var index = $(this).data('tabIndex')
+    //     tabContentSwiper.slideTo(index)
+    // })
+
+    // Hammer 能避免 zepto tap被异常触发的问题
+    tabHeader.each(function(index) {
+        tabNavItem = new Hammer(tabHeader[index]);
+        tabNavItem.on('tap', function(e) {
+            var node = $(e.target),
+                index = node.data('tabIndex') || node.closest('li').data('tabIndex') || -1
+            index > -1 && tabContentSwiper.slideTo(index)
+        })
     })
 
 
-    var scrollY = tabContent.find('div.scroll-y');
-    scrollY.each(function (index) {
-    	// console.log(scrollY.eq(index)[0])
-    	var swiper = new Swiper(scrollY.eq(index), {
-    	    // scrollbar: '.swiper-scrollbar',
-    	    direction: 'vertical',
-    	    slidesPerView: 'auto',
-    	    mousewheelControl: true,
-    	    freeMode: true
-    	});
 
-    	scrollY.eq(index).data('swiper', swiper)
-    })
+    function initScroller() {
+        var scrollY = tabContent.find('div.scroll-y');
+        for (var index = 0, length = scrollY.length; index < length; index++) {
+
+            var fn = function(index) {
+                return function() {
+                    var options = {
+                        scrollbar: scrollY.eq(index).children('.swiper-scrollbar'),
+                        direction: 'vertical',
+                        slidesPerView: 'auto',
+                        mousewheelControl: true,
+                        freeMode: true
+                    }
+                    var swiper = new Swiper(scrollY.eq(index), options);
+
+                    scrollY.eq(index).data('swiper', swiper)
+                }
+            }(index);
+
+            setTimeout(fn, 20)
+        }
+        // scrollY.each(function(index) {
+        //     // console.log(scrollY.eq(index)[0])
+        //     var swiper = new Swiper(scrollY.eq(index), {
+        //         // scrollbar: '.swiper-scrollbar',
+        //         direction: 'vertical',
+        //         slidesPerView: 'auto',
+        //         mousewheelControl: true,
+        //         freeMode: true
+        //     });
+
+        //     scrollY.eq(index).data('swiper', swiper)
+        // })
+    }
 
 
-// setTimeout(function () {
-	
-// alert(scrollY.eq(0)[0].clientHeight)
-// alert(scrollY.eq(0).find('.swiper-slide')[0].clientHeight)
-// }, 2000)
-}();
+
+    // $(function() {
+    // setTimeout(function() {
+    //     initScroller()
+    // }, 1000);
+    initScroller()
+        // });
+
+    // setTimeout(function () {
+
+    // alert(scrollY.eq(0)[0].clientHeight)
+    // alert(scrollY.eq(0).find('.swiper-slide')[0].clientHeight)
+    // }, 2000)
+}(window, document, $, Hammer);

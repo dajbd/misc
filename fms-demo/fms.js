49d642228cee4c5f35a42dc4c76a6f8130613199
Engin <denganjun1234@qq.com>
2016-05-18 20:35:56 +0800

    dist 2016-5-18 20:35:52

--- fms-demo/fms.js
+++
@@ -1,1 +0,0 @@
-var fms=require("fms");fms.run({CORS:!0,port:8080,ajax:{dataType:"json"}}),fms.ajax({url:"/test/",type:"get",res:{ok:"yes",err:"no"}}),fms.ajax({url:"/user",type:"post",res:{ok:{msg:"操作成功！",code:1},err:{msg:"操作失败！",code:0}}});
